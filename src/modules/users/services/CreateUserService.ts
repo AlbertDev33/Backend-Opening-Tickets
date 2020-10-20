3858986caa207ea2e405f31ea83d71332247e3e4
AlbertDev33 <alber725@gmail.com>
2020-10-19 21:48:02 -0300

    Service com a regra de negócio para criação de um usuário

---
+++ src/modules/users/services/CreateUserService.ts
@@ -0,0 +1,35 @@
+import { getCustomRepository } from 'typeorm';
+import { hash } from 'bcrypt';
+
+import User from '@modules/users/infra/typeorm/entities/User';
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+
+interface IRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  public async execute({ name, email, password }: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const checkUserExists = await usersRepository.findByEmail(email);
+
+    if (checkUserExists) {
+      throw new Error('Email address already used.');
+    }
+
+    const hashedPassword = await hash(password, 10);
+
+    const user = await usersRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    return user;
+  }
+}
+
+export default CreateUserService;
