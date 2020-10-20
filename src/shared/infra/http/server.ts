bb8436f6ae1b61cf64efd271173dc9e9e1cb8fa5
AlbertDev33 <alber725@gmail.com>
2020-10-19 21:52:57 -0300

    Tratamento das rotas da aplicação incluído. Variável routes

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,11 +2,14 @@ import 'reflect-metadata';
 
 import express from 'express';
 
+import routes from '@shared/infra/http/routes';
+
 import '@shared/infra/typeorm';
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('🚀 Executando na porta 3333');
