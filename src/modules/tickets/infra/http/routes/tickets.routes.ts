bee48e2d1e19f687d77efdca8e2045dac87b5ebd
AlbertDev33 <alber725@gmail.com>
2020-10-21 16:32:48 -0300

    Na rota get foi incluído autenticação, tryCatch e buscar todos os usuários. Na rota post o id do usuário está vindo do request.user, incluído na sobrescrição das tipagens do Request. Incluída a rota delete.

--- src/modules/tickets/infra/http/routes/tickets.routes.ts
+++ src/modules/tickets/infra/http/routes/tickets.routes.ts
@@ -1,12 +1,27 @@
 import { Router } from 'express';
 
 import CreateTicketService from '@modules/tickets/services/CreateTicketService';
+import ListAllTicketsService from '@modules/tickets/services/ListAllTicketsService';
+import DeleteTicketService from '@modules/tickets/services/DeleteTicketService';
+import confirmAuthenticated from '@modules/users/infra/http/middlewares/confirmAuthenticated';
 
 const usersRouter = Router();
 
-usersRouter.post('/:user_id', async (request, response) => {
+usersRouter.get('/', confirmAuthenticated, async (request, response) => {
   try {
-    const { user_id } = request.params;
+    const findAllTickets = new ListAllTicketsService();
+
+    const tickets = await findAllTickets.execute();
+
+    return response.json(tickets);
+  } catch (err) {
+    return response.status(err.statusCode).json({ error: err.message });
+  }
+});
+
+usersRouter.post('/', confirmAuthenticated, async (request, response) => {
+  try {
+    const { id } = request.user;
     const { subject, message } = request.body;
 
     const createTicket = new CreateTicketService();
@@ -14,10 +29,24 @@ usersRouter.post('/:user_id', async (request, response) => {
     const ticket = await createTicket.execute({
       subject,
       message,
-      user_id,
+      user_id: id,
     });
 
     return response.json(ticket);
+  } catch (err) {
+    return response.status(err.statusCode).json({ error: err.message });
+  }
+});
+
+usersRouter.delete('/', confirmAuthenticated, async (request, response) => {
+  try {
+    const { id } = request.body;
+
+    const updateTicket = new DeleteTicketService();
+
+    await updateTicket.execute(id);
+
+    return response.send();
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
