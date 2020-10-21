3b6c2c922f060e2776561bae6ba99ca3e92a19c2
AlbertDev33 <alber725@gmail.com>
2020-10-21 18:22:17 -0300

    Incluída a rota get autenticada

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,8 +1,26 @@
 import { Router } from 'express';
 
 import CreateUserService from '@modules/users/services/CreateUserService';
+import FindUserService from '@modules/users/services/FindUserService';
+import confirmAuthenticated from '@modules/users/infra/http/middlewares/confirmAuthenticated';
 
 const usersRouter = Router();
+
+usersRouter.get('/', confirmAuthenticated, async (request, response) => {
+  try {
+    const { id } = request.user;
+
+    const findUser = new FindUserService();
+
+    const user = await findUser.execute({ id });
+
+    delete user.password;
+
+    return response.json(user);
+  } catch (err) {
+    return response.status(err.statusCode).json({ error: err.message });
+  }
+});
 
 usersRouter.post('/', async (request, response) => {
   try {
