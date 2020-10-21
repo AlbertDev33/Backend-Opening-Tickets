e3217482faf6b150190a1fa722f6ae957f99255a
AlbertDev33 <alber725@gmail.com>
2020-10-21 18:20:30 -0300

    Incluído o método findById para buscar usuário por id

--- src/modules/users/repositories/IUsersRepository.ts
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -3,5 +3,6 @@ import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
 
 export default interface IUsersRepository {
   create(data: ICreateUserDTO): Promise<User>;
+  findById(id: string): Promise<User | undefined>;
   findByEmail(email: string): Promise<User | undefined>;
 }
