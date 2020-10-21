28111dc0ee3b60e5769fd78ee0811f6d6a452de5
AlbertDev33 <alber725@gmail.com>
2020-10-21 18:21:20 -0300

    Incluído o método findById

--- src/modules/users/infra/typeorm/repositories/UsersRepository.ts
+++ src/modules/users/infra/typeorm/repositories/UsersRepository.ts
@@ -13,6 +13,12 @@ class UsersRepository implements IUsersRepository {
     this.ormRepository = getRepository(User);
   }
 
+  public async findById(id: string): Promise<User | undefined> {
+    const user = await this.ormRepository.findOne(id);
+
+    return user;
+  }
+
   public async findByEmail(email: string): Promise<User | undefined> {
     const user = this.ormRepository.findOne({ where: { email } });
 
