77d51e98d694e1ddb113deabe220a0392141dc06
AlbertDev33 <alber725@gmail.com>
2020-10-21 22:47:30 -0300

    Método execute alterado para receber o id do usuário logado. Refatorado para desacoplar o typeorm. Aplicando dependency inversion principle

--- src/modules/tickets/services/ListTicketService.ts
+++ src/modules/tickets/services/ListTicketService.ts
@@ -1,19 +1,31 @@
-import { getCustomRepository } from 'typeorm';
-
 import Ticket from '@modules/tickets/infra/typeorm/entities/Ticket';
-import TicketsRepository from '@modules/tickets/infra/typeorm/repositories/TicketsRepository';
+import ITicketsRepository from '@modules/tickets/repositories/ITicketsRepository';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 
 import AppError from '@shared/errors/AppError';
 
 interface IRequest {
-  id: string;
+  user_id: string;
+  ticket_id: string;
 }
 
 class CreateTicketService {
-  public async execute({ id }: IRequest): Promise<Ticket | undefined> {
-    const ticketRepository = getCustomRepository(TicketsRepository);
+  constructor(
+    private ticketsRepository: ITicketsRepository,
+    private usersRepository: IUsersRepository,
+  ) {}
 
-    const listTicket = ticketRepository.findById(id);
+  public async execute({
+    user_id,
+    ticket_id,
+  }: IRequest): Promise<Ticket | undefined> {
+    const user = await this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('User not found', 404);
+    }
+
+    const listTicket = await this.ticketsRepository.findById(ticket_id);
 
     if (!listTicket) {
       throw new AppError('Ticket not found', 404);
