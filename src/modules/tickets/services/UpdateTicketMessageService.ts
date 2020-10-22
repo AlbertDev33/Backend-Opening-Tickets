491eb280070350cadf5f1eeb7d618f71a5302b2a
AlbertDev33 <alber725@gmail.com>
2020-10-21 22:45:53 -0300

    Refatorado para desacoplar o typeorm. Aplicando dependency inversion principle

--- src/modules/tickets/services/UpdateTicketMessageService.ts
+++ src/modules/tickets/services/UpdateTicketMessageService.ts
@@ -1,8 +1,6 @@
-import { getCustomRepository } from 'typeorm';
-
 import Ticket from '@modules/tickets/infra/typeorm/entities/Ticket';
-import TicketsRepository from '@modules/tickets/infra/typeorm/repositories/TicketsRepository';
-import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+import ITicketsRepository from '@modules/tickets/repositories/ITicketsRepository';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 
 import AppError from '@shared/errors/AppError';
 
@@ -13,21 +11,23 @@ interface IRequest {
 }
 
 class UpdateTicketService {
+  constructor(
+    private ticketsRepository: ITicketsRepository,
+    private usersRepository: IUsersRepository,
+  ) {}
+
   public async execute({
     user_id,
     ticket_id,
     message,
   }: IRequest): Promise<Ticket> {
-    const ticketRepository = getCustomRepository(TicketsRepository);
-    const userRepository = getCustomRepository(UsersRepository);
-
-    const user = await userRepository.findById(user_id);
+    const user = await this.usersRepository.findById(user_id);
 
     if (!user) {
       throw new AppError('User not found', 404);
     }
 
-    const ticket = await ticketRepository.findById(ticket_id);
+    const ticket = await this.ticketsRepository.findById(ticket_id);
 
     if (!ticket) {
       throw new AppError('Ticket not found');
@@ -35,7 +35,7 @@ class UpdateTicketService {
 
     ticket.message = message;
 
-    const newMessage = ticketRepository.save(ticket);
+    const newMessage = await this.ticketsRepository.update(ticket);
 
     return newMessage;
   }
