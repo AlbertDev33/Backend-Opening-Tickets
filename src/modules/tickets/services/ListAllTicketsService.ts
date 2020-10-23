3670bd90d820b549c3fde44b92220c5e14d06d9a
AlbertDev33 <alber725@gmail.com>
2020-10-23 16:57:25 -0300

    Repositório de users utilizado no construtor para recuperar o id do usuário.

--- src/modules/tickets/services/ListAllTicketsService.ts
+++ src/modules/tickets/services/ListAllTicketsService.ts
@@ -1,13 +1,27 @@
 import Ticket from '@modules/tickets/infra/typeorm/entities/Ticket';
 import ITicketsRepository from '@modules/tickets/repositories/ITicketsRepository';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 
 import AppError from '@shared/errors/AppError';
 
-class CreateTicketService {
-  constructor(private ticketsRepository: ITicketsRepository) {}
+interface IRequest {
+  user_id: string;
+}
 
-  public async execute(): Promise<Ticket[]> {
-    const listTickets = await this.ticketsRepository.findAllTickets();
+class CreateTicketService {
+  constructor(
+    private ticketsRepository: ITicketsRepository,
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  public async execute({ user_id }: IRequest): Promise<Ticket[]> {
+    const user = await this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('User not found', 404);
+    }
+
+    const listTickets = await this.ticketsRepository.findAllTickets(user_id);
 
     if (!listTickets) {
       throw new AppError('Ticket finished or not exisit', 404);
