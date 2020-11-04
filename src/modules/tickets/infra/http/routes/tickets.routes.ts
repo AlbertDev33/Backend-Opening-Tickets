d961a06541f627a382d9227bf64d543587276a32
AlbertDev33 <alber725@gmail.com>
2020-11-03 21:53:33 -0300

    Alteração das rotas para utilizar os Controllers

--- src/modules/tickets/infra/http/routes/tickets.routes.ts
+++ src/modules/tickets/infra/http/routes/tickets.routes.ts
@@ -1,120 +1,33 @@
 import { Router } from 'express';
 
-import CreateTicketService from '@modules/tickets/services/CreateTicketService';
-import ListTicketService from '@modules/tickets/services/ListTicketService';
-import ListAllTicketsService from '@modules/tickets/services/ListAllTicketsService';
-import DeleteTicketService from '@modules/tickets/services/DeleteTicketService';
-import UpdateTicketMessageService from '@modules/tickets/services/UpdateTicketMessageService';
-
-import TicketsRepository from '@modules/tickets/infra/typeorm/repositories/TicketsRepository';
-import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+import TicketsController from '@modules/tickets/infra/http/controllers/TicketsController';
+import TicketsUpdateController from '@modules/tickets/infra/http/controllers/TicketsUpdateController';
+import ListTicketsController from '@modules/tickets/infra/http/controllers/ListTicketsController';
 
 import confirmAuthenticated from '@modules/users/infra/http/middlewares/confirmAuthenticated';
 
 const ticketsRouter = Router();
 
+const ticketsController = new TicketsController();
+const ticketsUpdateController = new TicketsUpdateController();
+const listTicketsController = new ListTicketsController();
+
 ticketsRouter.get(
   '/:ticket_id',
   confirmAuthenticated,
-  async (request, response) => {
-    try {
-      const { ticket_id } = request.params;
-
-      const ticketsRepository = new TicketsRepository();
-
-      const findTicket = new ListTicketService(ticketsRepository);
-
-      const ticket = await findTicket.execute(ticket_id);
-
-      return response.json(ticket);
-    } catch (err) {
-      return response.status(err.statusCode).json({ error: err.message });
-    }
-  },
+  listTicketsController.index,
 );
 
-ticketsRouter.get('/', confirmAuthenticated, async (request, response) => {
-  try {
-    const { id } = request.user;
+ticketsRouter.get('/', confirmAuthenticated, ticketsController.index);
 
-    const ticketsRepository = new TicketsRepository();
-    const usersRepository = new UsersRepository();
-
-    const findAllTickets = new ListAllTicketsService(
-      ticketsRepository,
-      usersRepository,
-    );
-
-    const tickets = await findAllTickets.execute({ user_id: id });
-
-    return response.json(tickets);
-  } catch (err) {
-    return response.status(401).json({ error: err.message });
-  }
-});
-
-ticketsRouter.post('/', confirmAuthenticated, async (request, response) => {
-  try {
-    const { id } = request.user;
-    const { subject, message } = request.body;
-
-    const ticketsRepository = new TicketsRepository();
-    const createTicket = new CreateTicketService(ticketsRepository);
-
-    const ticket = await createTicket.execute({
-      subject,
-      message,
-      user_id: id,
-    });
-
-    return response.json(ticket);
-  } catch (err) {
-    return response.status(err.statusCode).json({ error: err.message });
-  }
-});
+ticketsRouter.post('/', confirmAuthenticated, ticketsController.create);
 
 ticketsRouter.patch(
   '/:ticket_id',
   confirmAuthenticated,
-  async (request, response) => {
-    try {
-      const { id } = request.user;
-      const { ticket_id } = request.params;
-      const { message } = request.body;
-
-      const ticketsRepository = new TicketsRepository();
-      const usersRepository = new UsersRepository();
-      const updateTicketMessage = new UpdateTicketMessageService(
-        ticketsRepository,
-        usersRepository,
-      );
-
-      const ticket = await updateTicketMessage.execute({
-        user_id: id,
-        ticket_id,
-        message,
-      });
-
-      return response.json(ticket);
-    } catch (err) {
-      return response.status(400).json({ error: err.message });
-    }
-  },
+  ticketsUpdateController.update,
 );
 
-ticketsRouter.delete('/', confirmAuthenticated, async (request, response) => {
-  try {
-    const { id } = request.body;
-
-    const ticketsRepository = new TicketsRepository();
-    const updateTicket = new DeleteTicketService(ticketsRepository);
-
-    await updateTicket.execute(id);
-
-    return response.send();
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
-});
+ticketsRouter.delete('/', confirmAuthenticated, ticketsController.delete);
 
 export default ticketsRouter;
