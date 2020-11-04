eccd4b17e631f9750b2535fde09edc1b2663142c
AlbertDev33 <alber725@gmail.com>
2020-11-03 21:50:20 -0300

    Alteração das rotas para utilizar os Controllers

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,49 +1,14 @@
 import { Router } from 'express';
 
-import CreateUserService from '@modules/users/services/CreateUserService';
-import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
-import FindUserService from '@modules/users/services/FindUserService';
+import UsersController from '@modules/users/infra/http/controllers/UsersCrontroller';
 
 import confirmAuthenticated from '@modules/users/infra/http/middlewares/confirmAuthenticated';
 
 const usersRouter = Router();
+const usersController = new UsersController();
 
-usersRouter.get('/', confirmAuthenticated, async (request, response) => {
-  try {
-    const { id } = request.user;
+usersRouter.get('/', confirmAuthenticated, usersController.index);
 
-    const usersRepository = new UsersRepository();
-    const findUser = new FindUserService(usersRepository);
-
-    const user = await findUser.execute({ id });
-
-    delete user.password;
-
-    return response.json(user);
-  } catch (err) {
-    return response.status(err.statusCode).json({ error: err.message });
-  }
-});
-
-usersRouter.post('/', async (request, response) => {
-  try {
-    const { name, email, password } = request.body;
-
-    const usersRepository = new UsersRepository();
-    const createUser = new CreateUserService(usersRepository);
-
-    const user = await createUser.execute({
-      name,
-      email,
-      password,
-    });
-
-    delete user.password;
-
-    return response.json(user);
-  } catch (err) {
-    return response.status(err.statusCode).json({ error: err.message });
-  }
-});
+usersRouter.post('/', usersController.create);
 
 export default usersRouter;
