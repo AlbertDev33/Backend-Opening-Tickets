0618c4a2124f9da50a27c92b2950ac585d45ba93
AlbertDev33 <alber725@gmail.com>
2020-11-05 15:26:04 -0300

    Declaração do provider de hash de senha

--- src/modules/users/infra/http/controllers/SessionsController.ts
+++ src/modules/users/infra/http/controllers/SessionsController.ts
@@ -2,13 +2,16 @@ import { Request, Response } from 'express';
 
 import SessionsUserService from '@modules/users/services/SessionsUserService';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+import BCryptHashProvider from '@modules/users/providers/implementations/BCryptHashProvider';
 
 export default class SessionsController {
   public async create(request: Request, response: Response): Promise<Response> {
     const { email, password } = request.body;
 
     const usersRepository = new UsersRepository();
-    const sesssionUser = new SessionsUserService(usersRepository);
+    const hashProvider = new BCryptHashProvider();
+
+    const sesssionUser = new SessionsUserService(usersRepository, hashProvider);
 
     const { user, token } = await sesssionUser.execute({
       email,
