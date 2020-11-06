eb2da2a826466f5f0ce32f394924e80d8f523cb6
AlbertDev33 <alber725@gmail.com>
2020-11-06 17:09:07 -0300

    Implementação do controller de reset de senha

---
+++ src/modules/users/infra/http/controllers/ResetPasswordController.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from 'express';
+
+import ResetPasswordService from '@modules/users/services/ResetPasswordService';
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
+import BCryptHashProvider from '@modules/users/providers/HashProvider/implementations/BCryptHashProvider';
+
+export default class ResetPasswordController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { token, password } = request.body;
+
+    const usersRepository = new UsersRepository();
+    const userTokensRepository = new UserTokensRepository();
+    const bcryptHashProvider = new BCryptHashProvider();
+
+    const resetPassword = new ResetPasswordService(
+      usersRepository,
+      userTokensRepository,
+      bcryptHashProvider,
+    );
+
+    await resetPassword.execute({
+      token,
+      password,
+    });
+
+    return response.status(204).json();
+  }
+}
