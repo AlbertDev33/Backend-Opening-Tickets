bbde2b63fca499323d450a2712185019387caf22
AlbertDev33 <alber725@gmail.com>
2020-11-06 17:29:49 -0300

    Atualização do path de IHashProvider

--- src/modules/users/services/SessionsUserService.ts
+++ src/modules/users/services/SessionsUserService.ts
@@ -2,7 +2,7 @@ import { sign } from 'jsonwebtoken';
 
 import User from '@modules/users/infra/typeorm/entities/User';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
-import IHashProvider from '@modules/users/providers/models/IHashProvider';
+import IHashProvider from '@modules/users/providers/HashProvider/models/IHashProvider';
 
 import authConfig from '@config/auth';
 import AppError from '@shared/errors/AppError';
@@ -20,6 +20,7 @@ interface IResponse {
 class SessionsUserService {
   constructor(
     private usersRepository: IUsersRepository,
+
     private hashProvider: IHashProvider,
   ) {}
 
