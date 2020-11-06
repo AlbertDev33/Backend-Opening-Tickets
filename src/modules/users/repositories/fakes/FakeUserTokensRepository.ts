1ddc1b4e2b91e9327f8dd55f5522d31208e85262
AlbertDev33 <alber725@gmail.com>
2020-11-06 17:21:58 -0300

    Implementação do método findByToken

--- src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
+++ src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -2,7 +2,7 @@ import { uuid } from 'uuidv4';
 
 import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
 
-import UserToken from '@modules/users/infra/typeorm/entities/UserTokens';
+import UserToken from '@modules/users/infra/typeorm/entities/UserToken';
 
 class FakeUserTokensRepository implements IUserTokensRepository {
   private userTokens: UserToken[] = [];
@@ -14,11 +14,21 @@ class FakeUserTokensRepository implements IUserTokensRepository {
       id: uuid(),
       token: uuid(),
       user_id,
+      created_at: new Date(),
+      updated_at: new Date(),
     });
 
     this.userTokens.push(userToken);
 
     return userToken;
+  }
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const findToken = this.userTokens.find(
+      userToken => userToken.token === token,
+    );
+
+    return findToken;
   }
 }
 
