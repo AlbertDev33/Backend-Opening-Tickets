175b22413ce33542ea32c4c18eff86f9bec1c218
AlbertDev33 <alber725@gmail.com>
2020-11-06 17:31:49 -0300

    Implementação da rota de password

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,11 +3,13 @@ import { Router } from 'express';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import ticketsRouter from '@modules/tickets/infra/http/routes/tickets.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
+import passwordRouter from '@modules/users/infra/http/routes/password.routes';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
 routes.use('/tickets', ticketsRouter);
 routes.use('/sessions', sessionsRouter);
+routes.use('/password', passwordRouter);
 
 export default routes;
