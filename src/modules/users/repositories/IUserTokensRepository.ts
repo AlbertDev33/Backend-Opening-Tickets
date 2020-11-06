89a28f5adbe983f1704d9f417555f3797ab83ae2
AlbertDev33 <alber725@gmail.com>
2020-11-06 17:19:04 -0300

    Implementação do método findByToken

--- src/modules/users/repositories/IUserTokensRepository.ts
+++ src/modules/users/repositories/IUserTokensRepository.ts
@@ -1,5 +1,6 @@
-import UserTokens from '@modules/users/infra/typeorm/entities/UserTokens';
+import UserToken from '@modules/users/infra/typeorm/entities/UserToken';
 
 export default interface IUserTokensRepository {
-  generate(user_id: string): Promise<UserTokens>;
+  generate(user_id: string): Promise<UserToken>;
+  findByToken(token: string): Promise<UserToken | undefined>;
 }
