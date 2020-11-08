78fa39debc8d9bc3b455ab2fbe52eafe3688b707
AlbertDev33 <alber725@gmail.com>
2020-11-08 13:21:41 -0300

    Implementado o HandlebarsMailTemplateProvider como parâmetro no EtherealMailProvider

--- src/modules/users/infra/http/controllers/ForgotPasswordController.ts
+++ src/modules/users/infra/http/controllers/ForgotPasswordController.ts
@@ -4,13 +4,17 @@ import SendForgotPasswordEmailService from '@modules/users/services/SendForgotPa
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
 import EtherealMailProvider from '@shared/providers/MailProvider/implementations/EtherealMailProvider';
+import HandlebarsMailTemplateProvider from '@shared/providers/MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
 
 export default class ForgotPasswordController {
   public async create(request: Request, response: Response): Promise<Response> {
     const { email } = request.body;
 
     const usersRepository = new UsersRepository();
-    const etherealMailProvider = new EtherealMailProvider();
+    const handlebarsMailTemplate = new HandlebarsMailTemplateProvider();
+    const etherealMailProvider = new EtherealMailProvider(
+      handlebarsMailTemplate,
+    );
     const userTokensRepository = new UserTokensRepository();
 
     const sesssionUser = new SendForgotPasswordEmailService(
