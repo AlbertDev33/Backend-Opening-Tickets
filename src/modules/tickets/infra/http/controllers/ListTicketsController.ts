a269625885640a09e755da215bbd47c3d042483c
AlbertDev33 <alber725@gmail.com>
2020-11-10 19:19:05 -0300

    Implementando a instância do RedisCacheProvider

--- src/modules/tickets/infra/http/controllers/ListTicketsController.ts
+++ src/modules/tickets/infra/http/controllers/ListTicketsController.ts
@@ -2,14 +2,19 @@ import { Request, Response } from 'express';
 
 import TicketsRepository from '@modules/tickets/infra/typeorm/repositories/TicketsRepository';
 import ListTicketService from '@modules/tickets/services/ListTicketService';
+import RedisCacheProvider from '@shared/providers/CacheProvider/implementations/RedisCacheProvider';
 
 export default class ListTicketsController {
   public async index(request: Request, response: Response): Promise<Response> {
     const { ticket_id } = request.params;
 
     const ticketsRepository = new TicketsRepository();
+    const redisCacheProvider = new RedisCacheProvider();
 
-    const findTicket = new ListTicketService(ticketsRepository);
+    const findTicket = new ListTicketService(
+      ticketsRepository,
+      redisCacheProvider,
+    );
 
     const ticket = await findTicket.execute(ticket_id);
 
