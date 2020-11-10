1fd6f41846cde9fe017b247067a459f05ef3c3fd
AlbertDev33 <alber725@gmail.com>
2020-11-10 19:15:41 -0300

    Implementação do cache para listar os tickets

--- src/modules/tickets/services/ListTicketService.ts
+++ src/modules/tickets/services/ListTicketService.ts
@@ -1,16 +1,27 @@
 import Ticket from '@modules/tickets/infra/typeorm/entities/Ticket';
 import ITicketsRepository from '@modules/tickets/repositories/ITicketsRepository';
+import ICacheProvider from '@shared/providers/CacheProvider/models/ICacheProvider';
 
-import AppError from '@shared/errors/AppError';
+// import AppError from '@shared/errors/AppError';
 
 class CreateTicketService {
-  constructor(private ticketsRepository: ITicketsRepository) {}
+  constructor(
+    private ticketsRepository: ITicketsRepository,
 
-  public async execute(ticket_id: string): Promise<Ticket | undefined> {
-    const listTicket = await this.ticketsRepository.findById(ticket_id);
+    private cacheProvider: ICacheProvider,
+  ) {}
+
+  public async execute(ticket_id: string): Promise<Ticket | null> {
+    let listTicket = await this.cacheProvider.recover<Ticket>(
+      `ticketList:${ticket_id}`,
+    );
 
     if (!listTicket) {
-      throw new AppError('Ticket not found', 404);
+      listTicket = await this.ticketsRepository.findById(ticket_id);
+
+      console.log('A query foi feita no banco');
+
+      await this.cacheProvider.save(`ticketList:${ticket_id}`, listTicket);
     }
 
     return listTicket;
