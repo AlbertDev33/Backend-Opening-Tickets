3b100a7bb079531613d14d703207361853ba877c
AlbertDev33 <alber725@gmail.com>
2020-11-18 20:48:29 -0300

    Passando o handlebars como parâmetro para a instância do EtherealMailProvider

--- src/shared/providers/index.ts
+++ src/shared/providers/index.ts
@@ -3,7 +3,11 @@ import { container } from 'tsyringe';
 import IMailProvider from '@shared/providers/MailProvider/models/IMailProvider';
 import EtherealMailProvider from '@shared/providers/MailProvider/implementations/EtherealMailProvider';
 
+import HandlebarsMailTemplateProvider from '@shared/providers/MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
+
+const handlebarsMailTemplate = new HandlebarsMailTemplateProvider();
+
 container.registerInstance<IMailProvider>(
   'MailProvider',
-  new EtherealMailProvider(),
+  new EtherealMailProvider(handlebarsMailTemplate),
 );
