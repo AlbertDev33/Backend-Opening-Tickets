806eb2ae891b3c31c222ef076e0710343a5df607
AlbertDev33 <alber725@gmail.com>
2020-12-09 10:51:19 -0300

    Entitie de Role

---
+++ src/modules/users/infra/typeorm/entities/Role.ts
@@ -0,0 +1,39 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Column,
+  ManyToMany,
+  JoinTable,
+} from 'typeorm';
+
+import Permission from '@modules/users/infra/typeorm/entities/Permission';
+
+@Entity('roles')
+class Role {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @ManyToMany(() => Permission, permission => permission.id)
+  @JoinTable({
+    name: 'permissions_roles',
+    joinColumns: [{ name: 'role_id' }],
+    inverseJoinColumns: [{ name: 'permission_id' }],
+  })
+  permissions: Permission[];
+}
+
+export default Role;
