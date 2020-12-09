b3e41e77b3b578253aaf942197a7e870e9f4127a
AlbertDev33 <alber725@gmail.com>
2020-12-09 10:51:48 -0300

    Entitie de Permission

---
+++ src/modules/users/infra/typeorm/entities/Permission.ts
@@ -0,0 +1,27 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Column,
+} from 'typeorm';
+
+@Entity('permissions')
+class Permission {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Permission;
