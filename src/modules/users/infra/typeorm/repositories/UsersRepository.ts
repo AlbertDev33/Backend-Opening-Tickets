e6ec4c47c1630073f2f826d75d8ee82fc04b90a4
AlbertDev33 <alber725@gmail.com>
2020-12-09 10:48:50 -0300

    Implementação do método findRole para retornar o nome da role como string

--- src/modules/users/infra/typeorm/repositories/UsersRepository.ts
+++ src/modules/users/infra/typeorm/repositories/UsersRepository.ts
@@ -4,12 +4,17 @@ import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
 
 import User from '@modules/users/infra/typeorm/entities/User';
+import Role from '@modules/users/infra/typeorm/entities/Role';
 
 class UsersRepository implements IUsersRepository {
   private ormRepository: Repository<User>;
 
+  private ormRepositoryRole: Repository<Role>;
+
   constructor() {
     this.ormRepository = getRepository(User);
+
+    this.ormRepositoryRole = getRepository(Role);
   }
 
   public async findById(id: string): Promise<User | undefined> {
@@ -24,15 +29,30 @@ class UsersRepository implements IUsersRepository {
     return user;
   }
 
+  public async findRole(name: string): Promise<string | undefined> {
+    const userRoles = await this.ormRepository.findOne(name, {
+      relations: ['roles'],
+    });
+
+    const roles = userRoles?.roles;
+    const role = roles?.map(r => r.name);
+
+    return role?.toString();
+  }
+
   public async create({
     name,
     email,
     password,
+    roles,
   }: ICreateUserDTO): Promise<User> {
+    const existsRole = await this.ormRepositoryRole.findByIds(roles);
+
     const user = this.ormRepository.create({
       name,
       email,
       password,
+      roles: existsRole,
     });
 
     await this.ormRepository.save(user);
