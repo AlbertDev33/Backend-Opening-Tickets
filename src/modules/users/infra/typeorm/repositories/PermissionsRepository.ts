710746b0eb404d2abcb5caa204a6a95f54b0eac1
AlbertDev33 <alber725@gmail.com>
2020-12-09 10:50:09 -0300

    Implementação do repositório de Permissions

---
+++ src/modules/users/infra/typeorm/repositories/PermissionsRepository.ts
@@ -0,0 +1,50 @@
+import { getRepository, Repository } from 'typeorm';
+
+import IPermissionsRepository from '@modules/users/repositories/IPermissionsRepository';
+import ICreatePermissionDTO from '@modules/users/dtos/ICreatePermissionDTO';
+
+import Permission from '@modules/users/infra/typeorm/entities/Permission';
+
+class PermissionRepository implements IPermissionsRepository {
+  private ormRepository: Repository<Permission>;
+
+  constructor() {
+    this.ormRepository = getRepository(Permission);
+  }
+
+  public async findByName(name: string): Promise<Permission | undefined> {
+    const findPermission = await this.ormRepository.findOne({ name });
+
+    return findPermission;
+  }
+
+  public async findById(id: string): Promise<Permission | undefined> {
+    const findPermission = await this.ormRepository.findOne(id);
+
+    return findPermission;
+  }
+
+  public async create({
+    name,
+    description,
+  }: ICreatePermissionDTO): Promise<Permission> {
+    const permission = this.ormRepository.create({
+      name,
+      description,
+    });
+
+    await this.ormRepository.save(permission);
+
+    return permission;
+  }
+
+  public async save(permission: Permission): Promise<Permission> {
+    return this.ormRepository.save(permission);
+  }
+
+  public async delete(permission: Permission): Promise<void> {
+    await this.ormRepository.delete(permission);
+  }
+}
+
+export default PermissionRepository;
