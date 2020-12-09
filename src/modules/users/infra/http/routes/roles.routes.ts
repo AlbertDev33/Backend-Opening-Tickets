181a47f838b6c0c5dfc71a1d63f61a11e4410361
AlbertDev33 <alber725@gmail.com>
2020-12-09 10:53:17 -0300

    Implementação da rota para criação da roles

---
+++ src/modules/users/infra/http/routes/roles.routes.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
+
+import RolesController from '@modules/users/infra/http/controllers/RolesController';
+
+const rolesRouter = Router();
+const rolesController = new RolesController();
+
+rolesRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      description: Joi.string().required(),
+      permissions: Joi.array().required(),
+    },
+  }),
+  rolesController.create,
+);
+
+export default rolesRouter;
