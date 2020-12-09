533c7398faf87136521d701b8a58168f84c38db4
AlbertDev33 <alber725@gmail.com>
2020-12-09 10:46:39 -0300

    Implementação do método findRole

--- src/modules/users/repositories/IUsersRepository.ts
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -1,9 +1,11 @@
-import User from '@modules/users/infra/typeorm/entities/User';
 import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
+
+import User from '@modules/users/infra/typeorm/entities/User';
 
 export default interface IUsersRepository {
   create(data: ICreateUserDTO): Promise<User>;
   findById(id: string): Promise<User | undefined>;
   findByEmail(email: string): Promise<User | undefined>;
+  findRole(name: string): Promise<string | undefined>;
   save(user: User): Promise<User>;
 }
