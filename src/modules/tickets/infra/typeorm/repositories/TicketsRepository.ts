babed54baa4cb5194388de18c059a1e31413bf0b
AlbertDev33 <alber725@gmail.com>
2020-12-09 10:58:28 -0300

    Retirada da condição se o findTicket é null

--- src/modules/tickets/infra/typeorm/repositories/TicketsRepository.ts
+++ src/modules/tickets/infra/typeorm/repositories/TicketsRepository.ts
@@ -21,12 +21,8 @@ class TicketsRepository implements ITicketsRepository {
     return tickets;
   }
 
-  public async findById(ticket_id: string): Promise<Ticket | null> {
+  public async findById(ticket_id: string): Promise<Ticket | undefined> {
     const findTicket = await this.ormRepository.findOne(ticket_id);
-
-    if (!findTicket) {
-      return null;
-    }
 
     return findTicket;
   }
