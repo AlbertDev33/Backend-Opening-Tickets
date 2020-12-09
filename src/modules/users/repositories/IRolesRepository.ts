eb3b9aad3f28a0a68d21774acbcab9e753d2a0ea
AlbertDev33 <alber725@gmail.com>
2020-12-09 10:47:16 -0300

    Métodos de Roles

---
+++ src/modules/users/repositories/IRolesRepository.ts
@@ -0,0 +1,9 @@
+import ICreateRoleDTO from '@modules/users/dtos/ICreateRoleDTO';
+import Role from '@modules/users/infra/typeorm/entities/Role';
+
+export default interface IRolesRepository {
+  create(data: ICreateRoleDTO): Promise<Role>;
+  save(role: Role): Promise<Role>;
+  delete(role: Role): Promise<void>;
+  findByName(name: string): Promise<Role | undefined>;
+}
