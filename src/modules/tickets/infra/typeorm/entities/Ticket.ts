d5ea842a3fd0ce749c21d4df2fceff3ec8c0c465
AlbertDev33 <alber725@gmail.com>
2021-01-13 20:04:39 -0300

    Implementação do relacionamento com usuário administrador e os campos user_role, accountabel, status, condition e conclusion

--- src/modules/tickets/infra/typeorm/entities/Ticket.ts
+++ src/modules/tickets/infra/typeorm/entities/Ticket.ts
@@ -6,6 +6,7 @@ import {
   UpdateDateColumn,
   ManyToOne,
   JoinColumn,
+  OneToOne,
 } from 'typeorm';
 
 import User from '@modules/users/infra/typeorm/entities/User';
@@ -27,6 +28,25 @@ class Ticket {
   @ManyToOne(() => User)
   @JoinColumn({ name: 'user_id' })
   user: User;
+
+  @Column()
+  user_role: string;
+
+  @Column()
+  accountable: string;
+
+  @OneToOne(() => User)
+  @JoinColumn({ name: 'accountable' })
+  admin: User;
+
+  @Column()
+  status: string;
+
+  @Column()
+  condition: string;
+
+  @Column()
+  conclusion: Date;
 
   @CreateDateColumn()
   created_at: Date;
