24356fa563828988229e8127d5d831012004709b
AlbertDev33 <alber725@gmail.com>
2021-01-13 19:57:25 -0300

    Implementado os campos user_role, accountable, status, condition e conclusion

--- src/modules/tickets/dtos/ICreateTicketDTO.ts
+++ src/modules/tickets/dtos/ICreateTicketDTO.ts
@@ -2,4 +2,9 @@ export default interface ICreateTicketDTO {
   subject: string;
   message: string;
   user_id: string;
+  user_role: string;
+  accountable?: string;
+  status: string;
+  condition: string;
+  conclusion?: Date;
 }
