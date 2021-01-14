b8d2c50d676df27e80a6efe16d8af79f3aa6d441
AlbertDev33 <alber725@gmail.com>
2021-01-14 19:06:16 -0300

    Implementação de tratamento de erro

--- src/modules/tickets/services/CreateTicketService.ts
+++ src/modules/tickets/services/CreateTicketService.ts
@@ -1,11 +1,15 @@
 import Ticket from '@modules/tickets/infra/typeorm/entities/Ticket';
 import ITicketsRepository from '@modules/tickets/repositories/ITicketsRepository';
+import AppError from '@shared/errors/AppError';
 import ICacheProvider from '@shared/providers/CacheProvider/models/ICacheProvider';
 
 interface IRequest {
   subject: string;
   message: string;
   user_id: string;
+  user_role: string;
+  status: string;
+  condition: string;
 }
 
 class CreateTicketService {
@@ -19,11 +23,21 @@ class CreateTicketService {
     subject,
     message,
     user_id,
+    user_role,
+    status,
+    condition,
   }: IRequest): Promise<Ticket> {
+    if (!status) {
+      throw new AppError('Invalid status', 406);
+    }
+
     const ticket = await this.ticketsRepository.create({
       subject,
       message,
       user_id,
+      user_role,
+      status,
+      condition,
     });
 
     await this.cacheProvider.invalidatePrefix('ticketList');
