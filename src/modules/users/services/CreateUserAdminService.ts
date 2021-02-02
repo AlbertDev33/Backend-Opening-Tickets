b5cabee750eecfb0e06e95e629affce74992f377
AlbertDev33 <alber725@gmail.com>
2021-02-01 21:49:54 -0300

    Service para criação de usuário administrador

---
+++ src/modules/users/services/CreateUserAdminService.ts
@@ -0,0 +1,70 @@
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import IRolesRepository from '@modules/users/repositories/IRolesRepository';
+import IHashProvider from '@shared/providers/HashProvider/models/IHashProvider';
+import User from '@modules/users/infra/typeorm/entities/User';
+import Role from '@modules/users/infra/typeorm/entities/Role';
+
+import AppError from '@shared/errors/AppError';
+
+interface IRequest {
+  user_id: string;
+  name: string;
+  email: string;
+  password: string;
+  roles: Role[];
+}
+
+class CreateUserAdminService {
+  constructor(
+    private usersRepository: IUsersRepository,
+
+    private roleReponsitory: IRolesRepository,
+
+    private hashProvider: IHashProvider,
+  ) {}
+
+  public async execute({
+    user_id,
+    name,
+    email,
+    password,
+    roles,
+  }: IRequest): Promise<User> {
+    const checkUserExists = await this.usersRepository.findByEmail(email);
+
+    if (checkUserExists) {
+      throw new AppError('E-mail address already used');
+    }
+
+    const checkUserAdmin = await this.usersRepository.findRole(user_id);
+
+    if (checkUserAdmin !== process.env.USER_ADMIN_ROLE) {
+      throw new AppError('User Unauthorized', 401);
+    }
+
+    const checkRoleAdmin = await this.roleReponsitory.findByName(roles);
+
+    if (!checkRoleAdmin) {
+      throw new AppError("Role don't exists!", 400);
+    }
+
+    const roleName = checkRoleAdmin.toString();
+
+    if (roleName !== process.env.USER_ADMIN_ROLE) {
+      throw new AppError('Only admin user can be registered!');
+    }
+
+    const hashedPassword = await this.hashProvider.generateHash(password);
+
+    const userAdmin = await this.usersRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+      roles,
+    });
+
+    return userAdmin;
+  }
+}
+
+export default CreateUserAdminService;
