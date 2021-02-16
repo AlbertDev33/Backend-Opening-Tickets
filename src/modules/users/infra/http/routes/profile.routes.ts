0616106c5c20a4044d6bc960650948a7ff4e5737
AlbertDev33 <alber725@gmail.com>
2021-02-16 15:37:47 -0300

    refactor: implement middleware admin in routes

--- src/modules/users/infra/http/routes/profile.routes.ts
+++ src/modules/users/infra/http/routes/profile.routes.ts
@@ -3,13 +3,14 @@ import { Router } from 'express';
 import ProfileController from '@modules/users/infra/http/controllers/ProfileController';
 
 import ensureAuthenticated from '@shared/infra/http/middlewares/confirmUserAuthenticated';
+import confirmAdminAuthenticated from '@shared/infra/http/middlewares/confirmAdminAuthenticated';
 
 const profileRouter = Router();
 const profileController = new ProfileController();
 
-profileRouter.use(ensureAuthenticated);
+profileRouter.use(ensureAuthenticated || confirmAdminAuthenticated);
 
-profileRouter.get('/', profileController.show);
+profileRouter.get('/:user_id', profileController.show);
 profileRouter.patch('/', profileController.update);
 
 export default profileRouter;
