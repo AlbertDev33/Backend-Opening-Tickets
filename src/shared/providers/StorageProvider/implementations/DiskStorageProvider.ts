4a4e3e9977d448768a3a80f70e6e229b65dc34cd
AlbertDev33 <alber725@gmail.com>
2021-02-16 15:35:36 -0300

    refactor: upload file with stream instead buffer

--- src/shared/providers/StorageProvider/implementations/DiskStorageProvider.ts
+++ src/shared/providers/StorageProvider/implementations/DiskStorageProvider.ts
@@ -1,15 +1,28 @@
 import fs from 'fs';
 import path from 'path';
+import { pipeline } from 'stream';
+import { promisify } from 'util';
+
 import uploadConfig from '@config/upload';
 
 import IStorageProvider from '@shared/providers/StorageProvider/models/IStorageProvider';
 
 class DiskStorageProvider implements IStorageProvider {
   public async saveFile(file: string): Promise<string> {
-    await fs.promises.rename(
-      path.resolve(uploadConfig.tmpFolder, file),
-      path.resolve(uploadConfig.uploadsFolder, file),
-    );
+    const tmpFolder = path.resolve(uploadConfig.tmpFolder, file);
+    const uploadsFolder = path.resolve(uploadConfig.uploadsFolder, file);
+
+    const promisedPipeline = promisify(pipeline);
+
+    const readStream = async () => {
+      await promisedPipeline(
+        fs.createReadStream(tmpFolder),
+        fs.createWriteStream(uploadsFolder),
+      );
+      await fs.promises.unlink(tmpFolder);
+    };
+
+    readStream().catch(err => err.message);
 
     return file;
   }
