854ccd2fe48fb62f464430a49f453dc07b5cf804
AlbertDev33 <alber725@gmail.com>
2021-02-21 21:17:05 -0300

    feat: generate random to ticket's identifier

--- src/shared/providers/HashProvider/implementations/BCryptHashProvider.ts
+++ src/shared/providers/HashProvider/implementations/BCryptHashProvider.ts
@@ -1,4 +1,4 @@
-import { hash, compare } from 'bcrypt';
+import { hash, compare, genSalt } from 'bcrypt';
 
 import IHashProvider from '@shared/providers/HashProvider/models/IHashProvider';
 
@@ -9,5 +9,11 @@ export default class BCryptHashProvider implements IHashProvider {
 
   public async compareHash(payload: string, hashed: string): Promise<boolean> {
     return compare(payload, hashed);
+  }
+
+  public async generateRandom(random: number): Promise<string> {
+    const indentifierHash = await genSalt(random);
+
+    return indentifierHash;
   }
 }
