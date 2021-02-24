0770f9df5736412352a094bef8f38c08a299c636
AlbertDev33 <alber725@gmail.com>
2021-02-24 19:54:46 -0300

    test: methods for tests

--- src/modules/users/repositories/fakes/FakeUsersRepository.ts
+++ src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -4,14 +4,28 @@ import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
 
 import User from '@modules/users/infra/typeorm/entities/User';
+import Role from '@modules/users/infra/typeorm/entities/Role';
 
 class FakeUsersRepository implements IUsersRepository {
   private users: User[] = [];
+
+  private roles: Role[] = [];
 
   public async findById(id: string): Promise<User | undefined> {
     const findUser = this.users.find(user => user.id === id);
 
     return findUser;
+  }
+
+  // Método inserido apenas no fake
+  public async findRoleById(role_id: Role[]): Promise<string | undefined> {
+    const parseRoleId = role_id.toString();
+
+    const findRole = this.roles.filter(role => role.id === parseRoleId);
+
+    const roleName = findRole.map(name => name.name);
+
+    return roleName.toString();
   }
 
   public async findByEmail(email: string): Promise<User | undefined> {
@@ -20,12 +34,13 @@ class FakeUsersRepository implements IUsersRepository {
     return findUser;
   }
 
-  public async findRole(role: string): Promise<string | undefined> {
-    const findRole = this.users.find(user => String(user.roles) === role);
+  public async findRole(user_id: string): Promise<string | undefined> {
+    const findRole = this.users.find(role => role.roles);
 
-    const nameUserRole = String(findRole);
+    const nameRole = findRole?.roles;
+    const roleName = nameRole?.map(name => name.name);
 
-    return nameUserRole;
+    return roleName?.toString();
   }
 
   public async create(userData: ICreateUserDTO): Promise<User> {
