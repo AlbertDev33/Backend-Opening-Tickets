4dd7da348732313d1f7901d8f1e79f74d229c3e5
AlbertDev33 <alber725@gmail.com>
2021-02-24 16:38:55 -0300

    refactor: replace paramter name

--- src/modules/users/repositories/IUsersRepository.ts
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -6,6 +6,6 @@ export default interface IUsersRepository {
   create(data: ICreateUserDTO): Promise<User>;
   findById(id: string): Promise<User | undefined>;
   findByEmail(email: string): Promise<User | undefined>;
-  findRole(name: string): Promise<string | undefined>;
+  findRole(user_id: string): Promise<string | undefined>;
   save(user: User): Promise<User>;
 }
