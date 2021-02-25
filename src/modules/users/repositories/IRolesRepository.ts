db33ecf1cb745880c75f5a8699347e430521d63d
AlbertDev33 <alber725@gmail.com>
2021-02-25 20:51:45 -0300

    refactor: better description and changed data type

--- src/modules/users/repositories/IRolesRepository.ts
+++ src/modules/users/repositories/IRolesRepository.ts
@@ -4,7 +4,7 @@ import Role from '@modules/users/infra/typeorm/entities/Role';
 export default interface IRolesRepository {
   create(data: ICreateRoleDTO): Promise<Role>;
   save(role: Role): Promise<Role>;
-  delete(role: Role): Promise<void>;
+  delete(role_id: string): Promise<void>;
   findAllRoles(): Promise<Role[]>;
   findByName(name: string): Promise<string | undefined>;
   findById(role_id: Role[]): Promise<string | undefined>;
