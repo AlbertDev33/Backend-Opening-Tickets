49ec616ae64e145daeeb509ab59c75b6848824b2
AlbertDev33 <alber725@gmail.com>
2021-02-25 20:49:22 -0300

    test: ensure that user avatar to be changed

---
+++ src/modules/users/services/UpdateUserAvatarService.spec.ts
@@ -0,0 +1,35 @@
+import FakeStorageProvider from '@shared/providers/StorageProvider/fakes/FakeStorageProvider';
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
+
+let fakeStorageProvider: FakeStorageProvider;
+let fakeUsersRepository: FakeUsersRepository;
+let updateUserAvatar: UpdateUserAvatarService;
+
+describe('UpdateUserAvatar', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeStorageProvider = new FakeStorageProvider();
+
+    updateUserAvatar = new UpdateUserAvatarService(
+      fakeUsersRepository,
+      fakeStorageProvider,
+    );
+  });
+
+  it('should be able to update a new avatar', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'john@example.com',
+      password: '123456',
+      roles: [],
+    });
+
+    await updateUserAvatar.execute({
+      user_id: user.id,
+      avatarFileName: 'avatar.png',
+    });
+
+    expect(user.avatar).toBe('avatar.png');
+  });
+});
