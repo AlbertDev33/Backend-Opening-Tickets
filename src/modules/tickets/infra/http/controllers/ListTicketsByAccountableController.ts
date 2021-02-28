5888c86ced3512f5b92c44ae28e61e9689ad3b71
AlbertDev33 <alber725@gmail.com>
2021-02-28 18:29:21 -0300

    refactor: add validation user

--- src/modules/tickets/infra/http/controllers/ListTicketsByAccountableController.ts
+++ src/modules/tickets/infra/http/controllers/ListTicketsByAccountableController.ts
@@ -2,14 +2,17 @@ import { Request, Response } from 'express';
 
 import ListTicketsByAccontableService from '@modules/tickets/services/ListTicketsByAccontableService';
 import TicketsRepository from '@modules/tickets/infra/typeorm/repositories/TicketsRepository';
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
 export default class ListTicketsByAccountableController {
   public async index(request: Request, response: Response): Promise<Response> {
     const { id } = request.user;
 
     const ticketRepository = new TicketsRepository();
+    const usersRepository = new UsersRepository();
 
     const listTicketsByAccountable = new ListTicketsByAccontableService(
+      usersRepository,
       ticketRepository,
     );
 
