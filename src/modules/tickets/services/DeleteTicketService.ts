7b96fd7c18bff09a466dbefb653c4f53988fad83
AlbertDev33 <alber725@gmail.com>
2021-02-28 18:32:30 -0300

    refactor: variable name changed

--- src/modules/tickets/services/DeleteTicketService.ts
+++ src/modules/tickets/services/DeleteTicketService.ts
@@ -5,14 +5,14 @@ import AppError from '@shared/errors/AppError';
 class DeleteTicketService {
   constructor(private ticketsRepository: ITicketsRepository) {}
 
-  public async execute(id: string): Promise<void> {
-    const ticket = await this.ticketsRepository.findById(String(id));
+  public async execute(ticket_id: string): Promise<void> {
+    const ticket = await this.ticketsRepository.findById(ticket_id);
 
     if (!ticket) {
       throw new AppError('Ticket not found', 404);
     }
 
-    await this.ticketsRepository.delete(id);
+    await this.ticketsRepository.delete(ticket_id);
   }
 }
 
