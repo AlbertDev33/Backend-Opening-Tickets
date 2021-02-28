6c3ccf45c5ed17490f953c4ee00808cf90a3419b
AlbertDev33 <alber725@gmail.com>
2021-02-28 20:19:42 -0300

    test: ensure that be possible update the message of a ticket with valide user

--- src/modules/tickets/services/UpdateTicketMessageService.spec.ts
+++ src/modules/tickets/services/UpdateTicketMessageService.spec.ts
@@ -2,7 +2,7 @@ import FakeTicketsRepository from '@modules/tickets/repositories/fakes/FakeTicke
 import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
 
 import UpdateTicketMessageService from '@modules/tickets/services/UpdateTicketMessageService';
-// import AppError from '@shared/errors/AppError';
+import AppError from '@shared/errors/AppError';
 
 let fakeTicketsRepository: FakeTicketsRepository;
 let fakeUsersRepository: FakeUsersRepository;
@@ -17,6 +17,27 @@ describe('UpdateTickeMessageService', () => {
       fakeTicketsRepository,
       fakeUsersRepository,
     );
+  });
+
+  it('should not be able to update a message of a ticket with invalid user_id', async () => {
+    const ticket = await fakeTicketsRepository.create({
+      identifier: 'fake_identifier',
+      message: 'Ticket Test',
+      subject: 'Create Ticket for Test',
+      user_id: 'fake_user_id',
+      user_role: 'fake_user_role',
+      condition: 'fake_condition',
+      status: 'fake_status',
+      conclusion: new Date(),
+    });
+
+    await expect(
+      updateTicketMessageService.execute({
+        user_id: 'non_exist',
+        ticket_id: ticket.id,
+        message: 'fake_message',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 
   it('should be able to update the message of a ticket', async () => {
